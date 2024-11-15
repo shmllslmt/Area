90e35ef1f1b616699fad4097024b73b191dad145
shmll <shmllslmt@gmail.com>
2024-11-15 14:32:30 +0800

    Scanner class

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     static double area = 0;
 
@@ -21,10 +23,28 @@ public class Main {
     // main module
     // execution starts from this module
     public static void main(String[] args) {
+        // Scanner object declaration
+        Scanner keyboard = new Scanner(System.in);
+
         // sequential control structure
         // declaration and initialisation of local variable
-        int length = 9;
-        double width = 100;
+        int length = 0;
+        double width = 0;
+        String name;
+
+        // prompt user for String value
+        System.out.print("What's your name? ");
+        name = keyboard.nextLine();
+        // displaying user input
+        System.out.println("Hello "+name);
+
+        // prompt user for int value
+        System.out.print("Enter length: ");
+        length = keyboard.nextInt();
+
+        // prompt user for double value
+        System.out.print("Enter width: ");
+        width = keyboard.nextDouble();
 
         // call the calcArea function to calculate the area of rectangle
         // length and width are passed to the calcArea module as parameters
